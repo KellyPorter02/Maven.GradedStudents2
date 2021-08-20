a0c29851c156fb6cd7188f6ab36657f02acd2393
Kelly A Porter <78833247+KellyPorter02@users.noreply.github.com>
2021-08-20 01:58:42 -0400

    student class tests

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,84 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Student {
+
+    private String firstName;
+    private String lastName;
+    private List<Double> examScores;
+
+    public Student(String firstName, String lastName, Double[] testScores) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        examScores = new ArrayList<Double>(Arrays.asList(testScores));
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getExamScores() {
+        // returns a string representation of all exams taken
+        // Exam Scores:
+        //	Exam 1 -> 100
+        //	Exam 2 -> 95
+        //	Exam 3 -> 123
+        //	Exam 4 -> 96
+        StringBuilder examScoreSb = new StringBuilder("Exam Scores: \n\t");
+        for (int i = 0; i < examScores.size(); i++) {
+            examScoreSb.append("Exam ")
+                    .append(i + 1)
+                    .append(" -> ")
+                    .append(examScores.get(i))
+                    .append("\n\t");
+        }
+        return examScoreSb.toString();
+    }
+
+    public int getNumberOfExamsTaken() {
+        return examScores.size();
+    }
+
+    public void addExamScore(double testScore) {
+        // adds an exam score to exam score list
+        examScores.add(testScore);
+    }
+
+    public void setExamScore(int examNumber, double newScore) {
+        // replaces the specified exam score with the new exam score
+        if (examNumber >= examScores.size()) {
+            throw new IllegalArgumentException("The student hasn't taken that many exams.");
+        }
+        if (examNumber <= 0) {
+            throw new IllegalArgumentException("Number of exam to reset must be 1 or above.");
+        }
+        examScores.set(examNumber - 1, newScore);
+    }
+
+    public double getAverageExamScore() {
+        // returns the sum of the examScore list, divided by size
+        return -1.0;
+    }
+
+    @Override
+    public String toString() {
+        return "Student Name: " + firstName + " " + lastName + "\n\t"
+                + "> Average Score: " + getAverageExamScore() + "\n\t"
+                + "> " + getExamScores();
+    }
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,141 @@
 package io.zipcoder;
 
+import org.junit.*;
+import org.junit.rules.ExpectedException;
+
 public class StudentTest {
 
+    Student testStudent;
+
+//    @Before
+//    public void setUp() throws Exception {
+//    }
+//
+//    @After
+//    public void tearDown() throws Exception {
+//    }
+
+    @Test
+    public void constructor() {
+    }
+
+    @Test
+    public void getFirstName() {
+    }
+
+    @Test
+    public void setFirstName() {
+    }
+
+    @Test
+    public void getLastName() {
+    }
+
+    @Test
+    public void setLastName() {
+    }
+
+    @Before
+    public void setUp1() {
+        Double[] scores = {100.0, 150.0, 250.0, 0.0};
+        testStudent = new Student("Leon", "Hunter", scores);
+    }
+
+    @Test
+    public void getExamScores() {
+        String expected = "Exam Scores: \n" +
+                "\tExam 1 -> 100.0\n" +
+                "\tExam 2 -> 150.0\n" +
+                "\tExam 3 -> 250.0\n" +
+                "\tExam 4 -> 0.0\n" +
+                "\t";
+        String returned = testStudent.getExamScores();
+        Assert.assertEquals(expected, returned);
+    }
+
+    @Test
+    public void getNumberOfExamsTaken() {
+        int expected = 4;
+        int returned = testStudent.getNumberOfExamsTaken();
+        Assert.assertEquals(expected,returned);
+    }
+
+    @Test
+    public void addExamScore() {
+        double scoreToAdd = 200.0;
+        String expected = "Exam Scores: \n" +
+                "\tExam 1 -> 100.0\n" +
+                "\tExam 2 -> 150.0\n" +
+                "\tExam 3 -> 250.0\n" +
+                "\tExam 4 -> 0.0\n" +
+                "\tExam 5 -> 200.0\n" +
+                "\t";
+        testStudent.addExamScore(scoreToAdd);
+        String returned = testStudent.getExamScores();
+        System.out.println(returned);
+        Assert.assertEquals(expected, returned);
+    }
+
+    @Test
+    public void setExamScore_resetFirstExamScore() {
+        int examToReset = 1;
+        double newExamScore = 175.0;
+        String expected = "Exam Scores: \n" +
+                "\tExam 1 -> 175.0\n" +
+                "\tExam 2 -> 150.0\n" +
+                "\tExam 3 -> 250.0\n" +
+                "\tExam 4 -> 0.0\n" +
+                "\t";
+        testStudent.setExamScore(examToReset, newExamScore);
+        String returned = testStudent.getExamScores();
+        Assert.assertEquals(expected, returned);
+    }
+
+    @Rule
+    public ExpectedException setExamScore_ArgTooHighRule = ExpectedException.none();
+
+    @Test
+    public void setExamScore_whenExamScoreArgTooHigh_thenIllegalArgExceptionThrown() {
+        setExamScore_ArgTooHighRule.expect(IllegalArgumentException.class);
+        int examToReset = 7;
+        double newExamScore = 45.0;
+        testStudent.setExamScore(examToReset, newExamScore);
+    }
+
+    @Test
+    public void setExamScore_whenExamScoreArgTooHigh_thenIllegalArgExceptionMessage() {
+        String messageExpected = "The student hasn't taken that many exams.";
+        setExamScore_ArgTooHighRule.expectMessage(messageExpected);
+        int examToReset = 7;
+        double newExamScore = 45.0;
+        testStudent.setExamScore(examToReset, newExamScore);
+    }
+
+    @Rule
+    public ExpectedException setExamScore_ArgTooLowRule = ExpectedException.none();
+
+    @Test
+    public void setExamScore_whenExamScoreTooLow_thenIllegalArgExceptionThrown() {
+        setExamScore_ArgTooLowRule.expect(IllegalArgumentException.class);
+        int examToReset = 0;
+        double newExamScore = 45.0;
+        testStudent.setExamScore(examToReset, newExamScore);
+    }
+
+    @Test
+    public void setExamScore_whenExamScoreTooLow_thenIllegalArgExceptionMessage() {
+        String messageExpected = "Number of exam to reset must be 1 or above.";
+        setExamScore_ArgTooLowRule.expectMessage(messageExpected);
+        int examToReset = 0;
+        double newExamScore = 45.0;
+        testStudent.setExamScore(examToReset, newExamScore);
+    }
+
+    @Test
+    public void getAverageExamScore() {
+    }
+
+    @Test
+    public void testToString() {
+    }
 }
